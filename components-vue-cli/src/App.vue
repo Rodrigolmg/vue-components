4524b797862d81d4573cc722bffe5be5c5b8091b
Rodrigolmg <rodrigo.leitemg@gmail.com>
2022-08-21 16:26:02 -0300

    Vue cli project with components

---
+++ components-vue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    {{ contador }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        contador: 1
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
